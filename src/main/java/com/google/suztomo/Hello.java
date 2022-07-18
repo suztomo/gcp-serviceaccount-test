e4ce4bcd9a250662d1ce74836e3c5ccf0ad7adfb
Tomo Suzuki <suztomo@google.com>
2022-07-18 21:07:17 +0000

    initial commit

---
+++ src/main/java/com/google/suztomo/Hello.java
@@ -0,0 +1,11 @@
+package com.google.suztomo;
+
+public class Hello {
+  public static void main(String[] arguments) {
+    System.out.println("Hello world!");
+  }
+
+  private Hello() {
+    
+  }
+}
